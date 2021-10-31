2962e95463d0d70d8f69bbfd32e02780191ca914
Jashema Panapa <53614792+ShemaxCodes@users.noreply.github.com>
2021-10-30 20:40:39 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
+'use strict';
 
+function logger() {
+    console.log('My name is Shema');
+}
+
+// calling / invoking the function
+logger();
+
+function fruitProcessor(apples, oranges) {
+    console.log(apples, oranges);
+    const juice = `Juice with ${apples} apples and ${oranges}
+    oranges.`;
+    return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
