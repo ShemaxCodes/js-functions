14dc29c2f8622fc9c5c316ce77c24d56242f11eb
Jashema Panapa <53614792+ShemaxCodes@users.noreply.github.com>
2021-10-30 20:47:26 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 'use strict';
 
+// these are FUNCTION DECLARATIONS
+
 function logger() {
     console.log('My name is Shema');
 }
@@ -19,3 +21,18 @@ console.log(appleJuice);
 
 const appleOrangeJuice = fruitProcessor(2, 4);
 console.log(appleOrangeJuice);
+
+function calcAge1(birthYear) {
+    return 2037 - birthYear;
+}
+
+
+// FUNCTION EXPRESSION
+const age1 = calcAge1(1988); // save it into a variable
+console.log(age1); //display it to the console
+
+const calcAge2 = function(birthYear) {
+    return 2037 - birthYear;
+}
+const age2 = calcAge2(1991);
+console.log(age1, age2);
